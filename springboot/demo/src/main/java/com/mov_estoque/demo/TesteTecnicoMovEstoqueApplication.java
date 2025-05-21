1b77fa3d8007f5e6fd8f87d642ed96e2ead9fc40
Renato Nunes <renato92.nunes@gmail.com>
2025-05-21 14:42:25 -0300

    first commit

---
+++ springboot/demo/src/main/java/com/mov_estoque/demo/TesteTecnicoMovEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.mov_estoque.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTecnicoMovEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteTecnicoMovEstoqueApplication.class, args);
+	}
+
+}
---
+++ springboot/demo/src/test/java/com/mov_estoque/demo/TesteTecnicoMovEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mov_estoque.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteTecnicoMovEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
